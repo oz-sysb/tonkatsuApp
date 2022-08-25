bcb77b11c7036277cda865f73516bc9a4704b349
ozv-y-amano <y-amano@oz-vision.co.jp>
2022-08-25 20:48:06 +0900

    add: mapと緯度経度取得までできた

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { LoadScript } from '@react-google-maps/api';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <LoadScript
+      libraries={['places']}
+      googleMapsApiKey={process.env.REACT_APP_API_KEY!}
+    >
+      <App />
+    </LoadScript>
   </React.StrictMode>
 );
