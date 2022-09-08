c961ab8d4f95747425c860975d4aae9f58a3d89a
ozv-y-amano <y-amano@oz-vision.co.jp>
2022-09-08 19:18:05 +0900

    add: マーカー表示までできた

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { GoogleMap, MarkerF } from '@react-google-maps/api';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const containerStyle = {
   width: '400px',
@@ -15,62 +15,53 @@ const center = {
 function App() {
   const [positions, setPositions] = useState<any[]>([]);
 
-  useEffect(() => {
-    var tokyo = new google.maps.LatLng(35.6936798374726, 139.76345088596605);
-
-    let map = new google.maps.Map(document.getElementById('map')!, {
-      center: tokyo,
-      zoom: 15,
-    });
+  const setTonkatsuPositions = async (map: google.maps.Map) => {
     var service = new google.maps.places.PlacesService(map);
 
-    service.nearbySearch(
-      {
-        keyword: 'とんかつ',
-        // wework神田
-        location: { lat: 35.6936798374726, lng: 139.76345088596605 },
-        radius: 300,
-      },
-      async function (results) {
-        const tmp: any = [];
-        console.log(
-          'results',
-          results,
-          'lat',
-          results![0].geometry?.location?.lat(),
-          'lng',
-          results![0].geometry?.location?.lng()
-        );
-        results?.forEach((result) => {
-          tmp.push({
-            lat: result.geometry?.location?.lat(),
-            lng: result.geometry?.location?.lng(),
+    const r = await new Promise<{ lat: number; lng: number }[]>((resolve) => {
+      service.nearbySearch(
+        {
+          keyword: 'とんかつ',
+          // wework神田
+          location: { lat: 35.6936798374726, lng: 139.76345088596605 },
+          radius: 300,
+        },
+        (results) => {
+          const tmp: { lat: number; lng: number }[] = [];
+          console.log(
+            'results',
+            results,
+            'lat',
+            results![0].geometry?.location?.lat(),
+            'lng',
+            results![0].geometry?.location?.lng()
+          );
+          results?.forEach((result) => {
+            tmp.push({
+              lat: result.geometry!.location!.lat(),
+              lng: result.geometry!.location!.lng(),
+            });
           });
-        });
-        console.log('a', tmp);
-        console.log('positions', positions);
-        await setPositions(tmp);
-      }
-    );
-  }, []);
+          resolve(tmp);
+        }
+      );
+    });
+    setPositions(r);
+  };
 
   return (
     <>
-      <div>
-        a
-        {positions.map((i, index) => (
-          <div key={index}>{i.lng as number}</div>
-        ))}
-      </div>
       <GoogleMap
         id="map"
         mapContainerStyle={containerStyle}
         center={center}
         zoom={17}
+        onLoad={(map) => {
+          setTimeout(() => setTonkatsuPositions(map));
+        }}
       >
-        {positions.map((i, index) => (
-          <MarkerF key={index} position={i} />
-        ))}
+        {positions.length > 0 &&
+          positions.map((i, index) => <MarkerF key={index} position={i} />)}
       </GoogleMap>
     </>
   );
