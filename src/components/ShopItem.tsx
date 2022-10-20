b9a3b8c974866d68c5f2e6759478315710749637
ozv-y-amano <y-amano@oz-vision.co.jp>
2022-10-20 19:19:16 +0900

    add: カードデザイン

--- src/components/ShopItem.tsx
+++ src/components/ShopItem.tsx
@@ -1,3 +1,5 @@
+import { Badge, Box, Image } from '@chakra-ui/react';
+import { StarIcon } from '@chakra-ui/icons';
 import { Data, Location } from '../App';
 
 type Props = {
@@ -7,14 +9,52 @@ type Props = {
 const ShopItem = ({ shop, currentPosition }: Props) => {
   return (
     <>
-      <div>{shop.name}</div>
-      <img src={shop.photo} alt={shop.name} />
-      <div>{shop.rating}</div>
-      <a
-        href={`https://www.google.com/maps/dir/?api=1&origin=${currentPosition?.lat}%2c${currentPosition?.lng}&destination=${shop.lat}%2c${shop.lng}&travelmode=walking`}
+      <Box
+        mb={15}
+        maxW="sm"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
       >
-        MAPを開く
-      </a>
+        <Image src={shop.photo} alt={shop.name} />
+
+        <Box p="6">
+          <Box display="flex" alignItems="baseline">
+            <Badge borderRadius="full" px="2" colorScheme="teal">
+              <a
+                href={`https://www.google.com/maps/dir/?api=1&origin=${currentPosition?.lat}%2c${currentPosition?.lng}&destination=${shop.lat}%2c${shop.lng}&travelmode=walking`}
+              >
+                MAPを開く
+              </a>
+            </Badge>
+          </Box>
+
+          <Box
+            mt="1"
+            fontWeight="semibold"
+            as="h4"
+            lineHeight="tight"
+            noOfLines={1}
+          >
+            {shop.name}
+          </Box>
+
+          <Box display="flex" mt="2" alignItems="center">
+            {Array(5)
+              .fill('')
+              .map((_, i) => (
+                <StarIcon
+                  key={i}
+                  color={i < shop.rating ? 'teal.500' : 'gray.300'}
+                />
+              ))}
+            <Box as="span" ml="2" color="gray.600" fontSize="sm">
+              {/* {property.reviewCount} reviews */}
+              {shop.rating}
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 };
